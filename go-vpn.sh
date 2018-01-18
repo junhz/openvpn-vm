2eea3aaa40023bc414d77bc86df05770aa3d1b27
junhz <junhz3721@gmail.com>
2018-01-18 13:48:45 +0000

    Update go-vpn.sh

--- go-vpn.sh
+++ go-vpn.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
 apt-get update
 apt-get --assume-yes install openvpn
-apt-get --assume-yes install easy-rsa
 apt-get --assume-yes install heirloom-mailx
-cd /usr/share/easy-rsa/
-mkdir keys
-touch keys/index.txt
-echo 01 > keys/serial
-source vars
-export KEY_EMAIL="$owner"
-./pkitool --initca
-./pkitool --server server
-./build-dh
-./pkitool client
-cd keys
-mv *.pem *.crt *.csr *.key /etc/openvpn
 cd /etc/openvpn
-wget --no-check-certificate https://raw.githubusercontent.com/junhz/openvpn-vm/master/server.conf
+openvpn --genkey --secret static.key
+wget --no-check-certificate https://raw.githubusercontent.com/junhz/openvpn-vm/static/server.conf
 echo "" >> server.conf
 echo "proto $protocol" >> server.conf
 echo "port $port" >> server.conf
-wget --no-check-certificate -O client.ovpn https://raw.githubusercontent.com/junhz/openvpn-vm/master/client.conf
+wget --no-check-certificate -O client.ovpn https://raw.githubusercontent.com/junhz/openvpn-vm/static/client.conf
 echo "" >> client.ovpn
 echo "proto $protocol" >> client.ovpn
 cat /etc/resolv.conf | grep "^nameserver" | sed "s/nameserver/dhcp-option DNS/" >> client.ovpn
 echo "remote $(dig +short myip.opendns.com @resolver1.opendns.com) $port" >> client.ovpn
-echo "<ca>" >> client.ovpn
-cat ca.crt | grep -A 100 "BEGIN CERTIFICATE" | grep -B 100 "END CERTIFICATE" >> client.ovpn
-echo "</ca>" >> client.ovpn
-echo "<cert>" >> client.ovpn
-cat client.crt | grep -A 100 "BEGIN CERTIFICATE" | grep -B 100 "END CERTIFICATE" >> client.ovpn
-echo "</cert>" >> client.ovpn
-echo "<key>" >> client.ovpn
-cat client.key | grep -A 100 "BEGIN PRIVATE KEY" | grep -B 100 "END PRIVATE KEY" >> client.ovpn
-echo "</key>" >> client.ovpn
+echo "<secret>" >> client.ovpn
+cat static.key | grep -A 100 "BEGIN OpenVPN Static key V1" | grep -B 100 "END OpenVPN Static key V1" >> client.ovpn
+echo "</secret>" >> client.ovpn
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 iptables -A FORWARD -i eth0 -o tun0 -m state --state ESTABLISHED,RELATED -j ACCEPT
